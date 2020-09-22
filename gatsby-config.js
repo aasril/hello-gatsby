3a4c6dc84c4c2653d407a2fa1c39ef705ae3be53
Ari Asril <airilsra@gmail.com>
2020-09-22 08:36:37 +0700

    Adjust config to use the blog theme and create first blog post

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,26 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: [],
+  siteMetadata: {
+    title: "My Blog",
+    author: "Ari Asril",
+    description: "My first and main blog.",
+    siteUrl: "https://pratama.gtsb.io",
+    social: [
+      {
+        name: "twitter",
+        url: "https://twitter.com/airilsra",
+      },
+      {
+        name: "github",
+        url: "https://github.com/aasril"
+      },
+    ],
+  },
+  plugins: [
+    {
+      resolve: "gatsby-theme-blog",
+      options: {},
+    },
+  ],
 }
